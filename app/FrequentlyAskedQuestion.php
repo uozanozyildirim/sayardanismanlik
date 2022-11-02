a28fdc4305569906e4dd7cde02dd3035d6c36d1d
Umut Ozan <umut@rgbilisim.com>
2022-11-02 13:09:32 +0300

    Members Details Changed

--- app/FrequentlyAskedQuestion.php
+++ app/FrequentlyAskedQuestion.php
@@ -5,12 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-
 class FrequentlyAskedQuestion extends Model
 {
-
     protected $table = 'frequently_asked_questions';
-
 
     public function getItems()
     {
